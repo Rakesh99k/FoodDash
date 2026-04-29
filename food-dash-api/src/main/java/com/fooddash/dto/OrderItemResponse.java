cce91078bcb793fcb14dc419e4d7ca14580add95
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2026-04-29 19:49:13 +0530

    changes

---
+++ food-dash-api/src/main/java/com/fooddash/dto/OrderItemResponse.java
@@ -0,0 +1,22 @@
+package com.fooddash.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemResponse {
+    private Long id;
+    private Long menuItemId;
+    private String menuItemName;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal lineTotal;
+    private String specialInstructions;
+}
