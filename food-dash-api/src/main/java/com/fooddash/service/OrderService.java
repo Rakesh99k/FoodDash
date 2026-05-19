4ad71c91646e0baa0f0f88e94913378d9b814af9
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2026-05-19 21:20:17 +0530

    changes

--- food-dash-api/src/main/java/com/fooddash/service/OrderService.java
+++ food-dash-api/src/main/java/com/fooddash/service/OrderService.java
@@ -3,17 +3,15 @@ package com.fooddash.service;
 import com.fooddash.dto.OrderCreateRequest;
 import com.fooddash.dto.OrderResponse;
 import com.fooddash.dto.OrderStatusUpdateRequest;
-import com.fooddash.model.User;
-
 import java.util.List;
 
 public interface OrderService {
 
-    OrderResponse createOrder(User user, OrderCreateRequest request);
+    OrderResponse createOrder(OrderCreateRequest request);
 
-    List<OrderResponse> getOrdersForUser(User user);
+    List<OrderResponse> getOrdersForUser();
 
-    OrderResponse getOrderDetails(Long orderId, User user);
+    OrderResponse getOrderDetails(Long orderId);
 
-    OrderResponse updateOrderStatus(Long orderId, OrderStatusUpdateRequest request, User user);
+    OrderResponse updateOrderStatus(Long orderId, OrderStatusUpdateRequest request);
 }
