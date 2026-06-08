5b8bb3a262dcff687c03b981f63a6269d76b1cda
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2026-06-08 20:08:24 +0530

    changes

--- food-dash-api/src/main/java/com/fooddash/service/NotificationService.java
+++ food-dash-api/src/main/java/com/fooddash/service/NotificationService.java
@@ -1,10 +1,21 @@
 package com.fooddash.service;
 
 import com.fooddash.model.FoodOrder;
+import com.fooddash.model.Payment;
 import java.time.Instant;
 import java.util.Map;
 
 public interface NotificationService {
+    void notifyOrderPlaced(FoodOrder order);
+
+    void notifyPaymentCompleted(Payment payment);
+
+    void notifyOrderConfirmed(FoodOrder order);
+
+    void notifyOutForDelivery(FoodOrder order);
+
+    void notifyDelivered(FoodOrder order);
+
     void notifyRestaurantOfNewOrder(FoodOrder order);
 
     void notifyCustomerOfOrderStatusChange(FoodOrder order);
