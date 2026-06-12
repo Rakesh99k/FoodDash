0c66596d25c7a229bd65ecd7643ac060cb897d3e
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2026-06-12 20:20:50 +0530

    changes

--- food-dash-api/src/test/java/com/fooddash/controller/AuthControllerTest.java
+++ food-dash-api/src/test/java/com/fooddash/controller/AuthControllerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fooddash.model.User;
 import com.fooddash.repository.UserRepository;
+import com.fooddash.support.AbstractIntegrationTest;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,17 +16,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-class AuthControllerTest {
-
-	@LocalServerPort
-	private int port;
+class AuthControllerTest extends AbstractIntegrationTest {
 
 	@Autowired
 	private UserRepository userRepository;
